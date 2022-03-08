6c036d5331923e3926dfcea0a72f089306012ed9
stiven45ruiz <stiven45ruiz@gmail.com>
2022-03-07 22:06:52 -0500

    Añadiendo productos al carrito

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { map } from 'rxjs/operators'
+
 import { CartService } from 'src/app/core/services/cart/cart.service';
 @Component({
   selector: 'app-header',
@@ -7,16 +11,15 @@ import { CartService } from 'src/app/core/services/cart/cart.service';
 })
 export class HeaderComponent implements OnInit {
 
-  total = 0;
+  total$: Observable<number>;
 
   constructor(
     private cartService: CartService
   ) {
-    this.cartService.cart$
-    .subscribe(products => {
-      console.log(products);
-      this.total = products.length;
-    })
+    this.total$ = this.cartService.cart$
+    .pipe(
+      map(products => products.length)
+    );
   }
 
   ngOnInit(): void {
